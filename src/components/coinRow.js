803485b9a203989f88f1e01c8607e81da6c5fffd
chamuh8 <115745993+chamuh8@users.noreply.github.com>
2023-02-09 14:45:37 -0300

    a

---
+++ src/components/coinRow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const CoinRow = ({coin,index}) => {
+    console.log(coin,index)
+  return (
+    <tr>
+        <td> {index}</td>
+        <td>{coin.name}</td>
+        <td>US {coin.current_price}</td>
+        <td>% {coin.price_change_percentage_24h}</td>
+        <td>{coin.total_volume}</td>
+    
+    </tr>
+  )
+}
+
+export default CoinRow
--- src/components/tableCoins.js
+++ src/components/tableCoins.js
@@ -1,4 +1,9 @@
 import React from "react";
+import CoinRow from "./CoinRow";
+
+const titles = ['#','Coin', 'Price', 'Price Change','24H Volume']
+
+
 
 const TableCoins = ({coins})=>{
 
@@ -7,17 +12,15 @@ const TableCoins = ({coins})=>{
         <table className="table table-dark mt-4 table-hover">
             <thead>
                 <tr>
-                    <td>
-                        className
-                    </td>
+                    {titles.map((title)=>
+                        <td>{title}</td>
+                    )}
                 </tr>
             </thead>
 
             <tbody>
-                {coins.map(coin=>(
-                    <tr key = {coin.name}>
-                        <td>{coin.name}</td>
-                    </tr>
+                {coins.map((coin,index)=>(
+                    <CoinRow coin={coin} key={index} index={index + 1} />
                 ))}
             </tbody>
 
