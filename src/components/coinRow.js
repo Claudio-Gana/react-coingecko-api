45fbb80ab9eaa4d1c7f44fcf12eba75ef8892ce8
chamuh8 <115745993+chamuh8@users.noreply.github.com>
2023-02-10 01:15:41 -0300

    arreglando visual

--- src/components/coinRow.js
+++ src/components/coinRow.js
@@ -6,7 +6,11 @@ const CoinRow = ({coin,index}) => {
   return (
     <tr>
         <td> {index}</td>
-        <td>{coin.name}</td>
+        <td>
+            <img src={coin.image} style={{width: '6%'}} className='img-fluid  me-4'/>
+            <span>{coin.name}</span>
+            <span className='ms-3 text-muted text-uppercase'>{coin.symbol}</span>
+            </td>
         <td>US {coin.current_price}</td>
         <td>% {coin.price_change_percentage_24h}</td>
         <td>{coin.total_volume}</td>
